b71cdecbb053cb60b0f248e49588ce01326e4e3c
Deep Sharma <145282452+dee-psharma@users.noreply.github.com>
2025-11-16 14:07:07 +0530

    Adding src

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Newsapp from './components/Newsapp'
+import './index.css';
+const App = () => {
+  return (
+    <div>
+      <Newsapp/>
+    </div>
+  )
+}
+
+export default App
